38c74d72f1dbb5fb9f9f76c8beb5c9013180eb25
NopalAul <mnaufal0605@gmail.com>
2023-09-19 10:55:03 +0700

    addMatrix

--- src/MainTemp.java
+++ src/MainTemp.java
@@ -1,8 +1,19 @@
 // nyoba akses method dari Matrix.java
 public class MainTemp {
     public static void main(String[] args) {
-        double[][] MatrixCurrent;
-        MatrixCurrent = Matrix.ReadMatrixKeyboard();
-        Matrix.DisplayMatrix(MatrixCurrent);
+        double[][] Matrix1, Matrix2, Matrix3;
+        Matrix1 = Matrix.ReadMatrixKeyboard();
+        Matrix.DisplayMatrix(Matrix1);
+        System.out.println();
+        Matrix2 = Matrix.ReadMatrixKeyboard();
+        Matrix.DisplayMatrix(Matrix2);
+
+        int baris = Matrix.getRow(Matrix1);
+        int kolom = Matrix.getCol(Matrix1);
+        System.out.println("baris: " + baris);
+        System.out.println("kolom: " + kolom);
+
+        Matrix3 = Matrix.addMatrix(Matrix1, Matrix2);
+        Matrix.DisplayMatrix(Matrix3);
     }
 }
--- src/Matrix.java
+++ src/Matrix.java
@@ -8,10 +8,33 @@ public class Matrix {
     public static Scanner scan;
 
     // constructor? belom
+    public static void CreateMatrix(int row, int col, double[][] matrix) {
+        for(int i = 0;i < row;i++) {
+            for(int j = 0;j < col;j++) {
+                matrix[i][j] = 0;
+            }
+        }
+    }
 
-    // selector
+    /* SELECTOR */
+    public static int getRow(double[][] matrix) {
+        return matrix.length;
+    }
+    
+    
+    public static int getCol(double[][] matrix) {
+        return matrix[0].length;
+    }
 
-    // method
+    public static int getLastIdxRow(double[][] matrix) {
+        return (getRow(matrix) - 1);
+    }
+
+    public static int getLastIdxCol(double[][] matrix) {
+        return (getCol(matrix) - 1);
+    }
+
+    /* METHOD */
     public static double[][] ReadMatrixKeyboard() {
         scan = new Scanner(System.in);
         System.out.print("Jumlah baris (row): "); int row = scan.nextInt();
@@ -40,5 +63,28 @@ public class Matrix {
                 }
             }
         }
+    }
+
+    // public static void copyMatrix(double[][] matrixIn, double[][] matrixOut) {
+    //     for(int i = 0;i < getRow(matrixIn);i++) {
+    //         for(int j = 0;j < getCol(matrixIn);j++) {
+    //             matrixOut[i][j] = matrixIn[i][j];
+    //         }
+    //     }
+    // }
+
+    public static double[][] addMatrix(double[][] matrix1, double[][] matrix2) {
+        /* Prekondisi : m1 berukuran sama dengan m2 */
+        /* Mengirim hasil penjumlahan matriks: m1 + m2 */
+        int row = getRow(matrix1);
+        int col = getCol(matrix1);
+        double[][] matrix = new double[row][col];
+
+        for(int i = 0; i < row; i++) {
+            for(int j = 0; j < col; j++) {
+                matrix[i][j] = matrix1[i][j] + matrix2[i][j];
+            }
+        }
+        return matrix;
     }
 }
